f1828fb27e08786d7fc339423ab6e18bf765a9bf
George Ndirangu <ndirangu.mepawa@gmail.com>
2019-12-06 01:18:53 +0300

    made ammends to factories, added counties, towns columns increased length of review column

--- database/migrations/2019_12_05_173719_create_shippers_table.php
+++ database/migrations/2019_12_05_173719_create_shippers_table.php
@@ -23,8 +23,8 @@ class CreateShippersTable extends Migration
             $table->string('address_line2')->nullable();
             $table->string('address_line3')->nullable();
             $table->string('phone', 12)->unique();
-            // $table->string('counties'); //TODO: manually create these as arrays in the db
-            // $table->string('towns');
+            $table->string('counties'); //TODO: manually create these as arrays in the db
+            $table->string('towns');
             $table->boolean('category_a')->nullable();
             $table->boolean('category_b')->nullable();
             $table->boolean('category_c')->nullable();
--- database/migrations/2019_12_05_183302_create_order_items_table.php
+++ database/migrations/2019_12_05_183302_create_order_items_table.php
@@ -17,7 +17,7 @@ class CreateOrderItemsTable extends Migration
             $table->id('order_itemID');
             $table->integer('price');
             $table->integer('rating')->default(0);
-            $table->string('review')->nullable();
+            $table->string('review', 1000)->nullable();
             $table->enum('order_status', ['pending', 'waiting to fulfil', 'waiting to ship', 'shipping', 'delivered', 'cancelled', 'returned', 'complete']);
             $table->bigInteger('productID');
             $table->bigInteger('orderID');
@@ -38,7 +38,6 @@ class CreateOrderItemsTable extends Migration
                 ->on('products')
                 ->onDelete('restrict')
                 ->onUpdate('cascade');
-
         });
     }
 
